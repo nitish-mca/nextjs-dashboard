4507871627bd81cb5b588cb813ebab96d5403cc0
Nitish Kumar <nitish.kumar1820@gmail.com>
2024-03-11 01:22:29 +0530

    adding authentication

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,14 +1,19 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { notFound } from 'next/navigation';
+
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
  
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-      ]);
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+      fetchInvoiceById(id),
+      fetchCustomers(),
+    ]);
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
